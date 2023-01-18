a12cdaba8028a309f0a363c41182c4122dfc423c
mech314 <31373450+mech314@users.noreply.github.com>
2023-01-17 23:13:27 -0700

    Refactored Armor and Weapon
    Learned how to make dataclasses and easier attribute assignment.

---
+++ game/global_vars.py
@@ -0,0 +1,9 @@
+item_modifier = {
+            'broken': 0.5,
+            'rusty': 0.6,
+            'simple': 0.8,
+            'normal': 1,
+            'excellent': 1.2,
+            'heroic': 1.3
+}
+
--- game/weapon.py
+++ game/weapon.py
@@ -1,179 +1,179 @@
 import random
+import global_vars
+from dataclasses import dataclass
+from typing import Dict, Tuple, NamedTuple, Any
 
 
+class WeaponStat(NamedTuple):
+    a: Tuple[int, int]
+    b: Tuple[int, int]
+
+
+_weapon_types = {
+    "sword": {
+        "damage": WeaponStat((1, 4), (3, 5)),
+        "durability": (15, 30),
+        "hp": (0, 1),
+        "luck": (1, 3),
+        "strength": (0, 0),
+        "agility": (-3, -1),
+        "movement": (-3, -1),
+        "intelligence": (1, 5),
+        "critical_chance": (0, 5),
+    },
+    "small sword": {
+        "damage": WeaponStat((3, 7), (7, 9)),
+        "durability": (25, 45),
+        "hp": (5, 15),
+        "luck": (1, 3),
+        "strength": (1, 4),
+        "agility": (0, 0),
+        "movement": (1, 3),
+        "intelligence": (0, 0),
+        "critical_chance": (0, 5),
+    },
+    "heavy sword": {
+        "damage": WeaponStat((2, 5), (6, 8)),
+        "durability": (15, 30),
+        "hp": (5, 10),
+        "luck": (0, 0),
+        "strength": (2, 5),
+        "agility": (-1, 3),
+        "movement": (1, 3),
+        "intelligence": (0, 0),
+        "critical_chance": (0, 5),
+    },
+    "axe": {
+        "damage": WeaponStat((1, 3), (4, 5)),
+        "durability": (15, 30),
+        "hp": (1, 5),
+        "luck": (1, 3),
+        "strength": (1, 3),
+        "agility": (-3, -1),
+        "movement": (-2, -1),
+        "intelligence": (0, 0),
+        "critical_chance": (0, 5),
+    },
+    "small axe": {
+        "damage": WeaponStat((1, 3), (4, 5)),
+        "durability": (15, 30),
+        "hp": (1, 5),
+        "luck": (1, 3),
+        "strength": (1, 3),
+        "agility": (-3, -1),
+        "movement": (-2, -1),
+        "intelligence": (0, 0),
+        "critical_chance": (0, 5),
+    },
+    "heavy axe": {
+        "damage": WeaponStat((1, 3), (4, 5)),
+        "durability": (15, 30),
+        "hp": (1, 5),
+        "luck": (1, 3),
+        "strength": (1, 3),
+        "agility": (-3, -1),
+        "movement": (-2, -1),
+        "intelligence": (0, 0),
+        "critical_chance": (0, 5),
+    }
+}
+
+
+@dataclass
 class Weapon:
     _id = 0
 
-    def __init__(self, name, condition=None, damage=None, durability=0, hp=0, luck=0, strength=0, agility=0,
-                 movement=0, intelligence=0, critical_chance=0, level=1, item_type='weapon', weapon_type=None,
-                 not_custom=True):
-        self.item_modifier = {'broken': 0.5, 'rusty': 0.6, 'simple': 0.8, 'normal': 1, 'excellent': 1.2,
-                              'heroic': 1.3}
-        if damage is None:
-            damage = [0, 0]
-        Weapon._id += 1
-        self._id = Weapon._id  # Need to finish this
-        self.name = name  # Name
-        self.condition = condition
-        self.damage = damage  # Amount of protection
-        self.durability = durability
-        self.hp = hp
-        self.luck = luck
-        self.strength = strength
-        self.agility = agility
-        self.movement = movement
-        self.intelligence = intelligence
-        self.critical_chance = critical_chance
-        self.level = level
-        self.item_type = item_type
-        self.weapon_type = weapon_type  # Important for putting armor on the character body parts
-        self.not_custom = not_custom
+    name: str
+    condition: Any = None
+    damage: Tuple[int, int] = (0, 0)
+    durability: int = 0
+    hp: int = 0
+    luck: int = 0
+    strength: int = 0
+    agility: int = 0
+    movement: int = 0
+    intelligence: int = 0
+    critical_chance: int = 0
+    level: int = 1
+    item_type: str = 'weapon'
+    weapon_type: str = ''
+    weapon_spec: str = ''   # Is used to add bonus for weapon specialisation
+    not_custom: bool = True
 
+    def __post_init__(self):
+        # Why not use a global variable for this?
+        Weapon._id += 1
+
+        if self.weapon_type == 'sword' or self.weapon_type == 'small sword' or self.weapon_type == 'heavy sword':
+            self.weapon_spec = 'sword'
+        elif self.weapon_type == 'axe' or self.weapon_type == 'small axe' or self.weapon_type == 'heavy axe':
+            self.weapon_spec = 'axe'
         if self.condition is None:  # If condition is 'fist' armor will be just naked.
-            self.damage = [0, 1]
+            self.damage = (0, 1)
             self.weapon_type = 'fist'
-        elif condition:
+        elif self.condition:
             self.weapon_maker()
 
+    def _gen_seed(self, lower: int, upper: int) -> int:
+        seed = random.randint(lower, upper)
+        item_stat = (self.level * global_vars.item_modifier[self.condition])
+        return int(seed * item_stat)
+
+    def _maker(self, _stats: Any) -> tuple[int, int] | int:
+        if isinstance(_stats, WeaponStat):
+            return self._gen_seed(*_stats.a), self._gen_seed(*_stats.b)
+        else:
+            return self._gen_seed(*_stats)
+
     def weapon_maker(self):
-        """This functions only for automatically generated items, Custom items will made by hand."""
-        if self.not_custom and self.weapon_type.lower() != 'fist':
-            if self.weapon_type.lower() == 'sword':
-                self.damage = [(int(random.randint(1, 4) * (self.level * self.item_modifier[self.condition]))),
-                               (int(random.randint(3, 5) * (self.level * self.item_modifier[self.condition])))]
-                self.durability = int((random.randint(15, 30) * (self.level * self.item_modifier[self.condition])))
-                self.hp = int((random.randint(0, 1) * (self.level * self.item_modifier[self.condition])))
-                self.luck = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.strength = int((random.randint(0, 0) * (self.level * self.item_modifier[self.condition])))
-                self.agility = int((random.randrange(-3, -1) * (self.level * self.item_modifier[self.condition])))
-                self.movement = int((random.randrange(-3, -1) * (self.level * self.item_modifier[self.condition])))
-                self.intelligence = int((random.randint(1, 5) * (self.level * self.item_modifier[self.condition])))
-                self.critical_chance = int((random.randint(0, 5) * (self.level * self.item_modifier[self.condition])))
-            elif self.weapon_type.lower() == 'small sword':
-                self.weapon_type = 'sword'
-                self.damage = [(int(random.randint(3, 7) * (self.level * self.item_modifier[self.condition]))),
-                               (int(random.randint(7, 9) * (self.level * self.item_modifier[self.condition])))]
-                self.durability = int((random.randint(25, 45) * (self.level * self.item_modifier[self.condition])))
-                self.hp = int((random.randint(0, 1) * (self.level * self.item_modifier[self.condition])))
-                self.luck = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.strength = int((random.randint(1, 4) * (self.level * self.item_modifier[self.condition])))
-                self.agility = int((random.randint(0, 0) * (self.level * self.item_modifier[self.condition])))
-                self.movement = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.intelligence = int((random.randint(0, 0) * (self.level * self.item_modifier[self.condition])))
-                self.critical_chance = int((random.randint(0, 5) * (self.level * self.item_modifier[self.condition])))
-            elif self.weapon_type.lower() == 'heavy sword':
-                self.weapon_type = 'sword'
-                self.damage = [(int(random.randint(2, 5) * (self.level * self.item_modifier[self.condition]))),
-                               (int(random.randint(6, 8) * (self.level * self.item_modifier[self.condition])))]
-                self.durability = int((random.randint(15, 30) * (self.level * self.item_modifier[self.condition])))
-                self.hp = int((random.randint(0, 1) * (self.level * self.item_modifier[self.condition])))
-                self.luck = int((random.randint(0, 0) * (self.level * self.item_modifier[self.condition])))
-                self.strength = int((random.randint(2, 5) * (self.level * self.item_modifier[self.condition])))
-                self.agility = int((random.randrange(-1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.movement = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.intelligence = int((random.randint(0, 0) * (self.level * self.item_modifier[self.condition])))
-                self.critical_chance = int((random.randint(0, 5) * (self.level * self.item_modifier[self.condition])))
-            elif self.weapon_type.lower() == 'axe':
-                self.weapon_type = 'axe'
-                self.damage = [(int(random.randint(1, 3) * (self.level * self.item_modifier[self.condition]))),
-                               (int(random.randint(4, 5) * (self.level * self.item_modifier[self.condition])))]
-                self.durability = int((random.randint(15, 30) * (self.level * self.item_modifier[self.condition])))
-                self.hp = int((random.randint(0, 1) * (self.level * self.item_modifier[self.condition])))
-                self.luck = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.strength = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.agility = int((random.randrange(-3, -1) * (self.level * self.item_modifier[self.condition])))
-                self.movement = int((random.randrange(-2, -1) * (self.level * self.item_modifier[self.condition])))
-                self.intelligence = int((random.randint(0, 0) * (self.level * self.item_modifier[self.condition])))
-                self.critical_chance = int((random.randint(0, 5) * (self.level * self.item_modifier[self.condition])))
-            elif self.weapon_type.lower() == 'small axe':
-                self.weapon_type = 'axe'
-                self.damage = [(int(random.randint(1, 4) * (self.level * self.item_modifier[self.condition]))),
-                               (int(random.randint(3, 5) * (self.level * self.item_modifier[self.condition])))]
-                self.durability = int((random.randint(15, 30) * (self.level * self.item_modifier[self.condition])))
-                self.hp = int((random.randint(0, 1) * (self.level * self.item_modifier[self.condition])))
-                self.luck = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.strength = int((random.randint(5, 7) * (self.level * self.item_modifier[self.condition])))
-                self.agility = int((random.randint(1, 7) * (self.level * self.item_modifier[self.condition])))
-                self.movement = int((random.randint(3, 9) * (self.level * self.item_modifier[self.condition])))
-                self.intelligence = int((random.randint(0, 0) * (self.level * self.item_modifier[self.condition])))
-                self.critical_chance = int((random.randint(0, 5) * (self.level * self.item_modifier[self.condition])))
-            elif self.weapon_type.lower() == 'heavy axe':
-                self.weapon_type = 'axe'
-                self.damage = [(int(random.randint(1, 4) * (self.level * self.item_modifier[self.condition]))),
-                              (int(random.randint(3, 5) * (self.level * self.item_modifier[self.condition])))]
-                self.durability = int((random.randint(15, 30) * (self.level * self.item_modifier[self.condition])))
-                self.hp = int((random.randint(0, 1) * (self.level * self.item_modifier[self.condition])))
-                self.luck = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.strength = int((random.randint(1, 3) * (self.level * self.item_modifier[self.condition])))
-                self.agility = int((random.randint(1, 6) * (self.level * self.item_modifier[self.condition])))
-                self.movement = int((random.randint(5, 10) * (self.level * self.item_modifier[self.condition])))
-                self.intelligence = int((random.randint(0, 0) * (self.level * self.item_modifier[self.condition])))
-                self.critical_chance = int((random.randint(0, 5) * (self.level * self.item_modifier[self.condition])))
+        """This functions only for automatically generated items, Custom items will be made by hand."""
+        if not self.not_custom and self.weapon_type.lower() != 'fist':
+            return
+
+        item_stats = _weapon_types.get(self.weapon_type.lower(), None)
+        if item_stats is None:
+            return
+
+        for name_stat, _stats in item_stats.items():
+            setattr(self, name_stat, self._maker(_stats))
 
     @property
     def weapon_chr(self):
-        """Creates the dictionary that has all characteristics of a weapon even if they are equal to 0."""
-        wpn = dict(name=self.name, level=self.level, damage=self.damage, durability=self.durability,
-                   condition=self.condition, luck=self.luck, strength=self.strength, agility=self.agility,
-                   movement=self.movement, intelligence=self.intelligence, critical_chance=self.critical_chance,
-                   weapon_type=self.weapon_type)
+        """Creates the dictionary that has all characteristics of an item even if they are equal to 0. However,
+        omits some attributes  like 'not_custom' etc."""
+        wpn = dict(
+            name=self.name,
+            level=self.level,
+            damage=self.damage,
+            durability=self.durability,
+            condition=self.condition,
+            luck=self.luck,
+            strength=self.strength,
+            agility=self.agility,
+            movement=self.movement,
+            intelligence=self.intelligence,
+            critical_chance=self.critical_chance,
+            weapon_type=self.weapon_type,
+            weapon_spec=self.weapon_spec,
+        )
         return {k.capitalize(): v for k, v in wpn.items()}
 
     @property
     def print_wpn_chr(self):
-        """Creates a dict that has only weapon values NOT equal to Zero, even if they are negative"""
-        wpn_chr = {key: value for (key, value) in self.weapon_chr.items() if value}
-        return {k.capitalize(): v for k, v in wpn_chr.items()}
-
-    # def __str__(self):
-    #     return str(self.name) + ' ' + str(self.damage) + ' ' + str(self.durability)
-    #
-    # def __repr__(self):
-    #     return str(self.name) + ' ' + str(self.damage) + ' ' + str(self.durability)
+        """"Show all attributes of an object"""
+        arm_chr = {key: value for (key, value) in self.__dict__.items() if value}
+        return {k.capitalize(): v for k, v in arm_chr.items()}
 
 
-# class Fist(Weapon):
-#
-#     def __init__(self, name, damage=None, weapon_type=None):
-#         super().__init__(damage, name, weapon_type)
-#         self.name = name
-#         self.damage = (1, 5)
-#         self.weapon_type = 'fist'
-#
-#
-# class Sword(Weapon):
-#
-#     def __init__(self, name, damage, durability, level=1, hp=0, luck=0, strength=0, agility=0,
-#                  movement=0, intelligence=0, critical_chance=0, weapon_type=None):
-#         super().__init__(name, damage, durability, level, hp, luck, strength, agility,
-#                          movement, intelligence, critical_chance, weapon_type)
-#         self.weapon_type = 'sword'
-#
-#
-# class Axe(Weapon):
-#
-#     def __init__(self, name, damage, durability, level=1, hp=0, luck=0, strength=0, agility=0,
-#                  movement=0, intelligence=0, critical_chance=0, weapon_type=None):
-#         super().__init__(name, damage, durability, level, hp, luck, strength, agility,
-#                          movement, intelligence, critical_chance, weapon_type)
-#         self.weapon_type = 'axe'
-#
-#
-# class Knife(Weapon):
-#
-#     def __init__(self, name, damage, durability, level=1, hp=0, luck=0, strength=0, agility=0,
-#                  movement=0, intelligence=0, critical_chance=0, weapon_type=None):
-#         super().__init__(name, damage, durability, level, hp, luck, strength, agility,
-#                          movement, intelligence, critical_chance, weapon_type)
-#         self.weapon_type = "knife"
-#
-#
-# class Bow(Weapon):
-#
-#     def __init__(self, name, damage, durability, level=1, hp=0, luck=0, strength=0, agility=0,
-#                  movement=0, intelligence=0, critical_chance=0, weapon_type=None):
-#         super().__init__(name, damage, durability, level, hp, luck, strength, agility,
-#                          movement, intelligence, critical_chance, weapon_type)
-#         self.weapon_type = "bow"
+if __name__ == '__main__':
+    weapon = Weapon('test1', 'excellent', weapon_type='sword')
+    print(weapon.weapon_chr)
+    print(weapon.print_wpn_chr)
 
+    weapon2 = Weapon('test2', 'excellent', weapon_type='small sword')
+    print(weapon2.weapon_chr)
+    print(weapon2.print_wpn_chr)
 
+    weapon3 = Weapon('test3', 'rusty', weapon_type='axe')
+    print(weapon3.weapon_chr)
+    print(weapon3.print_wpn_chr)
